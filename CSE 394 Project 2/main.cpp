1a93151d7c1ed8a97702fac7f63769c6e3d42a78
Derek Bixler <dbixler@Dereks-MacBook-Pro.local>
2014-11-02 04:06:47 -0700

    Initial Commit

---
+++ CSE 394 Project 2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CSE 394 Project 2
+//
+//  Created by Derek Bixler on 11/2/14.
+//  Copyright (c) 2014 Derek Bixler. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
